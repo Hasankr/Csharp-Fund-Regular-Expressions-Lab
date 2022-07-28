618b1b654bfc6f0b3f326d50177956972df27f26
hasankr <hasankr@abv.bg>
2022-07-28 23:04:14 +0300

    added 3

---
+++ 03. Match Dates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<day>[0-9]{2})([-\.\/])(?<month>[A-Z][a-z]{2})\1(?<year>[0-9]{4})";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
